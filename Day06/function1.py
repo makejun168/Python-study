368cd4a30bf9fa20ea9e9d1be996d750b7e23676
makejun168 <873800030@qq.com>
2019-08-25 00:04:35 +0800

    update

---
+++ Day06/function1.py
@@ -0,0 +1,19 @@
+# 函数的定义和使用 - 计算组合数C(7,3)
+
+# 阶乘函数
+
+
+def factorial(n):
+    result = 1
+    for num in range(1, n + 1):
+        result *= num
+    return result
+
+
+print(factorial(7))
+
+print(factorial(3))
+
+print(factorial(4))
+
+print(factorial(7) // factorial(3) // factorial(4))
