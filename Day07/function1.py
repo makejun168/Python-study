2e1d3275ae79b1402c7e10fc207e953e53b55c26
makejun168 <873800030@qq.com>
2019-08-30 16:19:33 +0800

    update

---
+++ Day07/function1.py
@@ -0,0 +1,48 @@
+str1 = 'hello world!'
+
+# 通过len函数计算字符串的长度
+print(len(str1))  # 12
+
+# 获得字符串首字母大写的拷贝
+print(str1.capitalize())  # Hello, world!
+
+# 获得字符串变大写后的拷贝
+print(str1.upper())  # HELLO, WORLD!
+
+# 从字符串中查找子串所在位置
+print(str1.find('or'))  # 7
+print(str1.find('shit'))  # -1
+
+# 与find类似但找不到子串时会引发异常
+print(str1.index('or'))  # 7
+# print(str1.index('shit'))
+
+# 检查字符串是否以指定的字符串开头
+print(str1.startswith('He'))  # False
+print(str1.startswith('hel'))  # True
+
+print((type(str1.startswith('He'))))
+print(isinstance(str1.startswith('HE'), bool))  # True
+
+# 检查字符串是否以指定的字符串结尾
+print(str1.endswith('!'))  # True
+
+# 将字符串以指定的宽度居中并在两侧填充指定的字符
+print(str1.center(50, 'f'))
+
+# 将字符串以指定的宽度靠右放置左侧填充指定的字符
+print(str1.rjust(50, ' '))
+
+str2 = 'abc123456'
+# 从字符串中取出指定位置的字符(下标运算)
+# 下标运算和 Javascript 一致
+print(str2[2])  # c
+
+
+# 字符串切片(从指定的开始索引到指定的结束索引)
+print(str2[2:5])  # c12
+print(str2[2:])  # c123456
+print(str2[2::2])  # c246
+print(str2[::2])  # ac246
+print(str2[::-1])  # 654321cba
+print(str2[-3:-1])  # 45
