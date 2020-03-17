f9980c03254c12b8056fd8dc7e7b5bfae0668361
makejun168 <873800030@qq.com>
2020-03-17 18:38:30 +0800

    update

--- Day08/guess.py
+++ Day08/guess.py
@@ -5,18 +5,17 @@
 from random import randint
 
 class guessNum(object):
-    def __init__(self):
-        print(object)
-        self.max = object.maxNum
-        self.min = object.minNum
-        self.answer = None
+    def __init__(self, max, min):
+        self._max = max
+        self._min = min
+        self._answer = None
 
     def guess(self, your_answer):
-        self.answer = randint(self.min, self.max)
-        if your_answer > self.answer:
+        self._answer = randint(self._min, self._max)
+        if your_answer > self._answer:
             print('大了一点')
             return False
-        elif your_answer < self.answer:
+        elif your_answer < self._answer:
             print('小了一点')
             return False
         else:
@@ -27,6 +26,7 @@ class guessNum(object):
         game_over = False
         while not game_over:
             your_answer = int(input('请输入你猜的数字:'))
+            print(your_answer)
             game_over = self.guess(self, your_answer)
 
 
